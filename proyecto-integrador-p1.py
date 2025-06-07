2b2e74a41fbaf8b47a6d0850d7c9905205405889
Karen Yanet Guardia <karen.yanet.guardia@gmail.com>
2025-06-07 00:47:13 -0300

    Realizamos el Bubble Sort para ordenar por nota y printeamos el orden.

--- proyecto-integrador-p1.py
+++ proyecto-integrador-p1.py
@@ -1,4 +1,4 @@
-# Armamos nuestra lista de estudiantes (cada uno es un diccionario)
+#Armamos nuestra lista de estudiantes (cada uno es un diccionario)
 
 estudiantes = [
     {"nombre": "Erika", "apellido": "Gonzalez", "nota": 9},
@@ -8,5 +8,19 @@ estudiantes = [
 ]
 
 
-# for estudiante in estudiantes:
-#      print(f"{estudiante['nombre']} {estudiante['apellido']} - Nota: {estudiante['nota']}")
+#for estudiante in estudiantes:
+#   print(f"{estudiante['nombre']} {estudiante['apellido']} - Nota: {estudiante['nota']}")
+
+#Usamos Bubble Sort para ordenar por nota
+def bubble_sort_nota(estudiantes):
+    n = len(estudiantes)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if estudiantes[j]["nota"] < estudiantes[j + 1]["nota"]: # > Ordena de menor a mayor < Ordena de mayor a menor
+                estudiantes[j], estudiantes[j + 1] = estudiantes[j + 1], estudiantes[j]
+
+#Printeamos para ver cómo estan ordenados por nota de mayor a menor:
+print("\nOrdenados por nota:")
+bubble_sort_nota(estudiantes)
+for estudiante in estudiantes:
+    print(f"{estudiante['nombre']} {estudiante['apellido']} - Nota: {estudiante['nota']}")
