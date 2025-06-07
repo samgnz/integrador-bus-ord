520c69a851cd0be3ce30478134eb746933521d1a
Karen Yanet Guardia <karen.yanet.guardia@gmail.com>
2025-06-06 21:39:22 -0300

    Comienzo armando listas paralelas y listas de listas.

---
+++ proyecto-integrador-p1.py
@@ -0,0 +1,12 @@
+nombres = ["Erika", "Karen", "Agustina", "Nicolás"]
+apellidos = ["Gonzalez", "Guardia", "Grille", "Gonzalez"]
+notas = [9, 8, 7.5, 8.5]
+
+estudiantes = [
+    ["Erika", "Gonzalez", 9],
+    ["Karen", "Guardia", 8],
+    ["Agustina", "Grille", 7.5],
+    ["Nicolás", "Gonzalez", 8.5]
+]
+
+print(estudiantes[0][0], estudiantes[0][1], estudiantes[0][2]) 
